7d72d0439aa3fa60a33624ca0ef8b0bca41e7e4f
Johann Hernandez <jhardnandez@gmail.com>
2022-01-26 08:13:22 -0600

    Fix bug 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://Hardnandez.github.io/',
+  base: 'https://Hardnandez.github.io/gh-react-portfolio',
   plugins: [react()]
 })
